624c5753b4f8157ef20b9a17833915776968bada
Emil VATAI <emil.vatai@gmail.com>
2023-07-19 19:34:44 +0900

    Remove packaging

--- bench/utils/__init__.py
+++ bench/utils/__init__.py
@@ -15,7 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from packaging import version
 
 from .. import __version__
 
@@ -23,11 +22,11 @@ WEIGHTS_NAME = "pytorch_model.bin"
 WEIGHTS_INDEX_NAME = "pytorch_model.bin.index.json"
 ADAPTER_WEIGHTS_NAME = "adapter_model.bin"
 ADAPTER_SAFE_WEIGHTS_NAME = "adapter_model.safetensors"
-#TF2_WEIGHTS_NAME = "tf_model.h5"
-#TF2_WEIGHTS_INDEX_NAME = "tf_model.h5.index.json"
-#TF_WEIGHTS_NAME = "model.ckpt"
-#FLAX_WEIGHTS_NAME = "flax_model.msgpack"
-#FLAX_WEIGHTS_INDEX_NAME = "flax_model.msgpack.index.json"
+# TF2_WEIGHTS_NAME = "tf_model.h5"
+# TF2_WEIGHTS_INDEX_NAME = "tf_model.h5.index.json"
+# TF_WEIGHTS_NAME = "model.ckpt"
+# FLAX_WEIGHTS_NAME = "flax_model.msgpack"
+# FLAX_WEIGHTS_INDEX_NAME = "flax_model.msgpack.index.json"
 SAFE_WEIGHTS_NAME = "model.safetensors"
 SAFE_WEIGHTS_INDEX_NAME = "model.safetensors.index.json"
 CONFIG_NAME = "config.json"
@@ -44,20 +43,3 @@ MULTIPLE_CHOICE_DUMMY_INPUTS = [
 ] * 2  # Needs to have 0s and 1s only since XLM uses it for langs too.
 DUMMY_INPUTS = [[7, 6, 0, 0, 1], [1, 2, 3, 0, 0], [0, 0, 0, 4, 5]]
 DUMMY_MASK = [[1, 1, 1, 1, 1], [1, 1, 1, 0, 0], [0, 0, 0, 1, 1]]
-
-
-def check_min_version(min_version):
-    if version.parse(__version__) < version.parse(min_version):
-        if "dev" in min_version:
-            error_message = (
-                "This example requires a source install from HuggingFace Transformers (see "
-                "`https://huggingface.co/docs/transformers/installation#install-from-source`),"
-            )
-        else:
-            error_message = f"This example requires a minimum version of {min_version},"
-        error_message += f" but the version found is {__version__}.\n"
-        raise ImportError(
-            error_message
-            + "Check out https://huggingface.co/transformers/examples.html for the examples corresponding to other "
-            "versions of HuggingFace Transformers."
-        )
