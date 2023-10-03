6382ca608a1cbaf5c86b1d786270809a8c36fba5
Aleksandr Drozd <alexander.drozd@gmail.com>
2023-10-03 13:39:52 +0900

    add perf monitor tool

--- distributed/megatron/training.py
+++ distributed/megatron/training.py
@@ -2,44 +2,37 @@
 
 """Pretrain utilities."""
 
-from datetime import datetime
 import math
 import sys
 import time
-# The earliest we can measure the start time.
-_TRAIN_START_TIME = time.time()
+from datetime import datetime
+
 import torch
+from megatron import (get_args, get_current_global_batch_size,
+                      get_num_microbatches, get_signal_handler,
+                      get_tensorboard_writer, get_timers, is_last_rank,
+                      print_rank_0, print_rank_last, update_num_microbatches)
+from megatron.checkpointing import load_checkpoint, save_checkpoint
+from megatron.core import mpu, tensor_parallel
+from megatron.core.enums import ModelType
+from megatron.core.pipeline_parallel import get_forward_backward_func
+from megatron.data.data_samplers import build_pretraining_data_loader
+from megatron.initialize import (initialize_megatron, set_jit_fusion_options,
+                                 write_args_to_tensorboard)
+from megatron.model import DistributedDataParallel as LocalDDP
+from megatron.model import Float16Module, GPTModel
+from megatron.model.vision.knn_monitor import compute_feature_bank
+from megatron.optimizer import get_megatron_optimizer
+from megatron.optimizer_param_scheduler import OptimizerParamScheduler
+from megatron.perf_monitor import perf_monitor
+from megatron.utils import (calc_params_l2_norm,
+                            check_adlr_autoresume_termination, report_memory,
+                            unwrap_model)
 from torch.nn.parallel.distributed import DistributedDataParallel as torchDDP
 
-from megatron import get_args
-from megatron import get_signal_handler
-from megatron import get_timers
-from megatron import get_tensorboard_writer
-from megatron import get_current_global_batch_size
-from megatron import get_num_microbatches
-from megatron import is_last_rank
-from megatron import update_num_microbatches
-from megatron.core import mpu, tensor_parallel
-from megatron import print_rank_0
-from megatron import print_rank_last
-from megatron.checkpointing import load_checkpoint
-from megatron.checkpointing import save_checkpoint
-from megatron.model import Float16Module
-from megatron.model import GPTModel
-from megatron.core.enums import ModelType
-from megatron.optimizer import get_megatron_optimizer
-from megatron.initialize import initialize_megatron
-from megatron.initialize import write_args_to_tensorboard
-from megatron.initialize import set_jit_fusion_options
-from megatron.optimizer_param_scheduler import OptimizerParamScheduler
-from megatron.model import DistributedDataParallel as LocalDDP
-from megatron.utils import check_adlr_autoresume_termination
-from megatron.utils import unwrap_model
-from megatron.data.data_samplers import build_pretraining_data_loader
-from megatron.utils import calc_params_l2_norm
-from megatron.core.pipeline_parallel import get_forward_backward_func
-from megatron.utils import report_memory
-from megatron.model.vision.knn_monitor import compute_feature_bank
+# The earliest we can measure the start time.
+_TRAIN_START_TIME = time.time()
+
 
 
 def print_datetime(string):
@@ -172,6 +165,7 @@ def pretrain(train_valid_test_dataset_provider,
                                    test_data_iterator, model,
                                    0, process_non_loss_data_func,
                                    True)
+
 
 def update_train_iters(args):
 
